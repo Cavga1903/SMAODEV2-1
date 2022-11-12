c8f872ab96aed090843c20dc3fff3e0546598320
Tolga <cavga228@gmail.com>
2022-11-12 13:59:42 +0300

    added

---
+++ Odev2/Controllers/HakkimdaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Odev2.Controllers
+{
+    public class HakkimdaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
